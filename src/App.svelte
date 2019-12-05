913c3dd6530a17835cc7f5c10fb1b06a6c53d83a
Cameron Sajedi <cameronsajedi@gmail.com>
2019-12-04 23:17:12 -0500

    working tailwind/svelte template

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Nav from './components/nav.svelte';
+    import Button from './components/Button.svelte';
+
+</script>
+
+<Nav />
---
+++ src/components/nav.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<nav class="shadow-outline flex items-center justify-between flex-wrap bg-blue-700 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+    
+        <span class="content-center font-sans font-semibold text-xl tracking-tight">MetaTransactions For Zilliqa</span>
+    </div>
+</nav>
